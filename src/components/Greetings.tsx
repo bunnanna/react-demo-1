b6b750f356188983fb2cbae5cf7ddff3cf18870c
bunnana <na.nuttanont@gmail.com>
2023-10-02 11:36:16 +0700

    add conditional rendering on Greeting

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -1,8 +1,18 @@
-const Greetings = ({ name }: { name: string }) => {
+interface IGreeting {
+  name: string
+  isLogin: boolean
+}
+const Greetings = ({ name, isLogin }: IGreeting) => {
   return (
-    <div className="w-64 h-auto border-solid border border-black rounded-lg flex justify-around items-center gap-2 p-5">
-      <div className=" text-lg text-red-600">Greetings</div>
-      <div className="text-base ">{name}</div>
+    <div className="w-64 h-20 border-solid border border-black rounded-lg flex justify-around items-center gap-2 p-5">
+      {isLogin ? (
+        <>
+          <div className=" text-lg text-red-600">Greetings</div>
+          <div className="text-base ">{name}</div>
+        </>
+      ) : (
+        <div>Unknown</div>
+      )}
     </div>
   )
 }
--- src/components/Maincontent.tsx
+++ src/components/Maincontent.tsx
@@ -1,8 +1,9 @@
 import Greetings from './Greetings'
 const Maincontent = () => {
   return (
-    <main className="flex-1 flex items-center justify-center">
-      <Greetings name="bun" />
+    <main className="flex-1 flex items-center justify-center gap-3">
+      <Greetings name="bun" isLogin />
+      <Greetings name="bun" isLogin={false} />
     </main>
   )
 }
