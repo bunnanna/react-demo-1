bab3a8bc79b521e3fea8cd814b4fa3a210b7f852
bunnana <na.nuttanont@gmail.com>
2023-10-02 09:26:14 +0700

    use tailwind

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -1,9 +1,8 @@
-import styleclass from './Greeting.module.css'
 const Greetings = () => {
   return (
-    <div className={styleclass.card}>
-      <div className={styleclass.header}>Greetings</div>
-      <div className={styleclass.body}>
+    <div className="w-64 h-auto border-solid border border-black rounded-md flex flex-col justify-around items-center gap-2 p-5">
+      <div className=" text-lg text-red-600">Greetings</div>
+      <div className="text-base ">
         Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
         perspiciatis repellat repudiandae nesciunt ullam nulla officiis error odit qui veniam.
       </div>
---
+++ src/components/Maincontent.tsx
@@ -0,0 +1,9 @@
+import Greetings from './Greetings'
+const Maincontent = () => {
+  return (
+    <main className="flex-1 flex items-center justify-center">
+      <Greetings />
+    </main>
+  )
+}
+export default Maincontent
