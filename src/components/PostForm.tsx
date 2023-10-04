1576bd3f8ae5eb5554e14c88ae65d2d32f2f18aa
bunnana <na.nuttanont@gmail.com>
2023-10-04 16:41:40 +0700

    fetch data and useeffect/demo

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-interface IFormData {
-  title: string
-  body: string
-}
-const PostForm = () => {
+import { IFormData, CreatePostDTO } from '../types/postdto'
+
+const PostForm = ({ onHandlePost }: { onHandlePost: (post: CreatePostDTO) => Promise<void> }) => {
   const [formData, setformData] = useState<IFormData>({ title: '', body: '' })
+  const [sending, setsending] = useState(false)
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setformData((prev) => {
       return { ...prev, [e.target.name]: e.target.value }
@@ -17,13 +16,18 @@ const PostForm = () => {
       <label htmlFor="body">body</label>
       <input type="text" value={formData.body} onChange={handleChange} name="body" />
       <button
-        className="border border-slate-950"
-        onClick={(e) => {
+        className="border border-slate-950 "
+        style={{ backgroundColor: `${sending ? 'red' : 'blue'}` }}
+        disabled={sending}
+        onClick={async (e) => {
           e.preventDefault()
+          setsending(true)
+          await onHandlePost({ ...formData, userId: 2 })
           setformData({ title: '', body: '' })
+          setsending(false)
         }}
       >
-        clear
+        Post
       </button>
     </form>
   )
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,33 +1,39 @@
-import { PostDTO } from '../types/postdto'
+import { useEffect, useState } from 'react'
+import { CreatePostDTO, PostDTO } from '../types/postdto'
 import Post from './Post'
 import PostForm from './PostForm'
-
-const posts: PostDTO[] = [
-  {
-    id: 1,
-    userId: 1,
-    title: "Let's learn React!",
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    id: 2,
-    userId: 2,
-    title: 'How to install Node.js',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    id: 3,
-    userId: 3,
-    title: 'Basic HTML',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-]
+import axios, { AxiosError } from 'axios'
 
 const PostList = () => {
+  const [posts, setposts] = useState<PostDTO[] | null>(null)
+  const [err, setErr] = useState<AxiosError | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get<PostDTO[]>('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => setposts(res.data))
+        .catch((err: AxiosError) => {
+          setErr(err)
+        })
+    }
+    fetchData()
+  }, [])
+
+  if (err) return <h1>{err.message}</h1>
+  if (!posts) return <p>Loading...</p>
+
+  const onHandlePost = async (post: CreatePostDTO) => {
+    return axios.post<PostDTO>('https://jsonplaceholder.typicode.com/posts', post).then((res) => {
+      setposts([res.data, ...posts])
+      console.log(res.data)
+    })
+  }
+
   return (
     <>
-      <PostForm />
-      <div className="flex gap-4 justify-center items-start my-10 mx-auto w-4/5">
+      <PostForm onHandlePost={onHandlePost} />
+      <div className="flex flex-col gap-4 justify-center items-start my-10 mx-auto w-4/5">
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
